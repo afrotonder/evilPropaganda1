bfeb25686b32d7df0d34e064a70c7554f3f39df4
Omar Rosado Ramírez <o.rosado18@gmail.com>
2020-02-20 16:07:39 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# evilPropaganda1
-Created with CodeSandbox
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+document.getElementById("app").innerHTML = `
+<div class="imgwrap">
+    <div class="img">
+      <h1> BEHOLD   &nbsp&nbsp  EVIL</h1>  
+    </div>
+
+</div>
+`;
